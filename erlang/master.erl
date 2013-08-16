366620100d21d84fe3cda4119a96224e68d4e134
Philipp Neugebauer <flippus@nexgo.de>
2013-08-16 21:37:56 +0200

    master task 1 implemented

---
+++ erlang/master.erl
@@ -0,0 +1,31 @@
+-module(master).
+-export([master/1]).
+
+% start
+% Master = spawn(masterslave,master,[[]]).
+
+master(Slaves) ->
+    receive
+        create ->
+            Child = spawn(fun slave/0),
+            master([Child|Slaves]);
+        {send, X} ->
+            lists:foreach(fun(N) -> N ! X end, Slaves),
+            master(Slaves);
+        kill ->
+            lists:foreach(fun(N) -> N ! terminate end, Slaves),
+            master(Slaves);
+        terminate ->
+            lists:foreach(fun(N) -> N ! terminate end, Slaves);
+        _ ->
+            master(Slaves)
+    end.
+
+slave() ->
+    receive
+        terminate -> 
+            io:format("~p terminates\n",[self()]);
+        X -> 
+            io:format("~p: ~p\n",[self(), X]),
+            slave()                
+    end.
